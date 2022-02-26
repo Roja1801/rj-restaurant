ea4cc046ef690e49bc77848ec2a8fb51ac8bff95
Roja <rojamuthu2001@gmail.com>
2022-02-26 15:04:11 +0530

    commit

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import {images,data} from '../../constants'
+import { SubHeading, MenuItem } from '../../components';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className='app__specialMenu flex center section__padding' id='menu'>
+    <div className='app__specialMenu-title'>
+      <SubHeading title="Menu that fits you palatte"/>
+    </div>
   </div>
 );
 
